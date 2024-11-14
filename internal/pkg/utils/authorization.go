bfc3b7c290cea69e9719a55e49ae10b635ae2751
nathakusuma <nathakusuma07@gmail.com>
2024-11-15 01:34:47 +0700

    feat(auth): add super admin bypass RequireKemenbiroID

--- internal/pkg/utils/authorization.go
+++ internal/pkg/utils/authorization.go
@@ -6,7 +6,12 @@ import (
 	"github.com/google/uuid"
 )
 
+// RequireKemenbiroID Dependencies: [middleware.Authenticate]
 func RequireKemenbiroID(ctx context.Context, kemenbiroID uuid.UUID) error {
+	if isSuperAdmin := ctx.Value("user.is_super_admin").(bool); isSuperAdmin {
+		return nil
+	}
+
 	userKemenbiroID, ok := ctx.Value("user.kemenbiro_id").(uuid.UUID)
 	if !ok {
 		return response.ErrForbiddenRole
