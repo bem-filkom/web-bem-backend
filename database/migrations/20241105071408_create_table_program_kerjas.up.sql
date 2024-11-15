cce434a681cb6d9be64cca40710fbe372b888039
nathakusuma <nathakusuma07@gmail.com>
2024-11-16 03:38:28 +0700

    fix(proker): change schema unique index to be not case-sensitive

--- database/migrations/20241105071408_create_table_program_kerjas.up.sql
+++ database/migrations/20241105071408_create_table_program_kerjas.up.sql
@@ -1,8 +1,10 @@
 CREATE TABLE program_kerjas
 (
     id           UUID PRIMARY KEY DEFAULT uuid_generate_v7(),
-    slug         VARCHAR(255) UNIQUE NOT NULL,
-    name         VARCHAR(255)        NOT NULL,
-    kemenbiro_id UUID                NOT NULL REFERENCES kemenbiros (id) ON DELETE SET NULL,
+    slug         VARCHAR(255) NOT NULL,
+    name         VARCHAR(255) NOT NULL,
+    kemenbiro_id UUID         NOT NULL REFERENCES kemenbiros (id) ON DELETE SET NULL,
     description  VARCHAR(2000)
 );
+
+CREATE UNIQUE INDEX program_kerjas_slug_key ON program_kerjas (UPPER(slug));
