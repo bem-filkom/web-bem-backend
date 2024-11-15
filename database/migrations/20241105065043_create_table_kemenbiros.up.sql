ba79ad70b37b5034f5866c19aa88cde5a199a6c1
nathakusuma <nathakusuma07@gmail.com>
2024-11-15 15:51:07 +0700

    fix(kemenbiro): fix abbreviation conflict case sensitive

--- database/migrations/20241105065043_create_table_kemenbiros.up.sql
+++ database/migrations/20241105065043_create_table_kemenbiros.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE kemenbiros
 (
     id           UUID PRIMARY KEY DEFAULT uuid_generate_v7(),
-    abbreviation VARCHAR(15) UNIQUE NOT NULL,
-    name         VARCHAR(255)       NOT NULL,
+    abbreviation VARCHAR(15)  NOT NULL,
+    name         VARCHAR(255) NOT NULL,
     description  VARCHAR(2000)
 );
 
-CREATE INDEX ON kemenbiros USING HASH (abbreviation);
+CREATE UNIQUE INDEX kemenbiros_abbreviation_key ON kemenbiros (UPPER(abbreviation));
